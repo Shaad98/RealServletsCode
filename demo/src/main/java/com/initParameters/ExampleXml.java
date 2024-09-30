94966164a8ba5087532408bc1ccb2ba0e66cdbdb
Shaad <141047163+Shaad98@users.noreply.github.com>
2024-09-30 21:59:03 +0530

    Initialization Parameter By XML Example 2

--- demo/src/main/java/com/initParameters/ExampleXml.java
+++ demo/src/main/java/com/initParameters/ExampleXml.java
@@ -4,12 +4,17 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 
 public class ExampleXml extends HttpServlet{
+    ServletConfig config;
+    public void init(ServletConfig config) throws ServletException
+    {
+        this.config = config;
+    }
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException
     {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        String message = getServletConfig().getInitParameter("message");
-        String secondMessage = getServletConfig().getInitParameter("secondMessage");
+        String message = config.getInitParameter("message");
+        String secondMessage = config.getInitParameter("secondMessage");
         out.println("<html>");
         out.println("<head>");
         out.println("<title>Response</title>");
